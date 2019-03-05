1d1c8dd58f24b1e3ca918b18bcec067796b82d52
asiamar <mariasik.joanna@gmail.com>
2019-03-05 23:18:39 +0100

    99bottlesofbear

---
+++ src/main/java/string.java
@@ -0,0 +1,22 @@
+
+public class string 
+{
+	public static void main(String[] args) 
+	{
+		String tekst1 = " bottles of beer on the wall, ";
+		String tekst2 = " bottles of beer.";
+		String tekst3 = "Take one down and pass it around - ";
+		String tekst4 = " bottles of beer on the wall.";
+		String tekst5 = "no more bottles of beer on the wall.";
+		
+	for (int x=99; x>=1 ;x--)
+	if (x>1)	
+	
+	{System.out.println(x+tekst1+x+tekst2+tekst3+(x-1)+tekst4);}
+		
+	else if (x==1)
+	
+    {System.out.println(x+tekst1+x+tekst2+tekst3+tekst5);}	
+	}
+}
+
